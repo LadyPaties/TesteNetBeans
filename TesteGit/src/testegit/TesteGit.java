de9525940b4ca3b1ead103407808e05f7ac9be1c
patricia_gageiro <patricia_gageiro@10.1.48.226>
2013-07-29 16:52:11 -0300

    Adicionando a impressão teste

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -15,5 +15,7 @@ public class TesteGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Teste");
     }
 }
